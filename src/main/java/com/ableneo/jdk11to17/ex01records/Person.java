e56e9c84fba8b691183ecd1a31511cfe55173738
Michal Boska <michal.boska@ableneo.com>
2023-06-06 18:02:56 +0200

    ex01 - Records - jdk14 - HasAge

--- src/main/java/com/ableneo/jdk11to17/ex01records/Person.java
+++ src/main/java/com/ableneo/jdk11to17/ex01records/Person.java
@@ -1,10 +1,15 @@
 package com.ableneo.jdk11to17.ex01records;
 
-public record Person(String name, String surname, int age) {
+public record Person(String name, String surname, int age) implements HasAge {
     public Person {
         if (age < 0) throw new IllegalArgumentException("Age must not be negative");
         name = name.trim();
         surname = surname.trim();
+    }
+
+    @Override
+    public int getAge() {
+        return age;
     }
 }
 
