bd9d7f1c966af89bb9e583beb3e80375e5fabc67
Michal Boska <michal.boska@ableneo.com>
2023-06-08 14:31:28 +0200

    ex07 - String api - indent - jdk 17

--- src/test/java/com/ableneo/jdk11to17/ex07stringapi/StringProcessorTest.java
+++ src/test/java/com/ableneo/jdk11to17/ex07stringapi/StringProcessorTest.java
@@ -1,9 +1,71 @@
 package com.ableneo.jdk11to17.ex07stringapi;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class StringProcessorTest {
 
-    // TODO switch to jdk17 branch
+    private static final String HTML_TEMPLATE = """
+            <html lang="en">
+            <head>
+                <title>E-Shop</title>
+            </head>
+            <body>
+            <h1>Welcome, John Doe!</h1>
+            </body>
+            </html>
+            """;
+
+    private static final String HTML_TEMPLATE_INDENTED_BY_2 = """
+              <html lang="en">
+              <head>
+                  <title>E-Shop</title>
+              </head>
+              <body>
+              <h1>Welcome, John Doe!</h1>
+              </body>
+              </html>
+            """;
+
+    private static final String HTML_TEMPLATE_UNINDENTED_BY_2 = """
+            <html lang="en">
+            <head>
+              <title>E-Shop</title>
+            </head>
+            <body>
+            <h1>Welcome, John Doe!</h1>
+            </body>
+            </html>
+            """;
+
+    private static final String HTML_TEMPLATE_UNINDENTED_AND_INDENTED_BY_2 = """
+              <html lang="en">
+              <head>
+                <title>E-Shop</title>
+              </head>
+              <body>
+              <h1>Welcome, John Doe!</h1>
+              </body>
+              </html>
+            """;
+
+    @Test
+    void testStringIndentAndUnindent() {
+        final String indented = HTML_TEMPLATE.indent(2);
+        final String unindented = indented.indent(-2);
+
+        assertThat(indented).isEqualTo(HTML_TEMPLATE_INDENTED_BY_2);
+        assertThat(unindented).isEqualTo(HTML_TEMPLATE);
+    }
+
+    @Test
+    void testStringUnindentAndIndent() {
+        final String unindented = HTML_TEMPLATE.indent(-2);
+        final String indented = unindented.indent(2);
+
+        assertThat(unindented).isEqualTo(HTML_TEMPLATE_UNINDENTED_BY_2);
+        assertThat(indented).isEqualTo(HTML_TEMPLATE_UNINDENTED_AND_INDENTED_BY_2);
+    }
 
 }
