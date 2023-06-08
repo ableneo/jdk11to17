b59591a85ab1a339cde104967dead9dd8c8a53d7
Michal Boska <michal.boska@ableneo.com>
2023-06-08 14:17:46 +0200

    ex07 - String api - jdk 17

---
+++ src/main/java/com/ableneo/jdk11to17/ex07stringapi/StringProcessor.java
@@ -0,0 +1,8 @@
+package com.ableneo.jdk11to17.ex07stringapi;
+
+public class StringProcessor {
+
+    public StringProcessor() {
+        throw new UnsupportedOperationException("Not supported in jdk11 version, please see tests in jdk17 branch");
+    }
+}
