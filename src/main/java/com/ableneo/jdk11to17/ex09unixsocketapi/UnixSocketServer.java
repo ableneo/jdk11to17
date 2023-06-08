d631dba31082ca411f3e2cc98e4ee0dae1b3c278
Michal Boska <michal.boska@ableneo.com>
2023-06-08 16:55:55 +0200

    ex09 - Unix socket api - jdk 17

--- src/main/java/com/ableneo/jdk11to17/ex09unixsocketapi/UnixSocketServer.java
+++ src/main/java/com/ableneo/jdk11to17/ex09unixsocketapi/UnixSocketServer.java
@@ -1,8 +1,49 @@
 package com.ableneo.jdk11to17.ex09unixsocketapi;
 
+import java.io.IOException;
+import java.net.StandardProtocolFamily;
+import java.net.UnixDomainSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class UnixSocketServer {
 
-    public static void main(String[] args) {
-        throw new UnsupportedOperationException("Not supported under jdk11. Please switch to jdk17 branch.");
+    public static void main(String[] args) throws IOException {
+        if (args.length < 1) {
+            throw new IllegalArgumentException("Socket path was not provided");
+        }
+        final Path socketPath = Paths.get(args[0]);
+        final UnixDomainSocketAddress socketAddress = UnixDomainSocketAddress.of(socketPath);
+        try (final ServerSocketChannel serverSocketChannel = ServerSocketChannel.open(StandardProtocolFamily.UNIX)) {
+            serverSocketChannel.configureBlocking(true);
+            Files.deleteIfExists(socketPath);
+            serverSocketChannel.bind(socketAddress);
+            try (final SocketChannel socketChannel = serverSocketChannel.accept()) {
+                final String message = readSocketMessage(socketChannel);
+                writeSocketMessage(socketChannel, "Hello, " + message);
+            }
+        } finally {
+            Files.deleteIfExists(socketPath);
+        }
+    }
+
+    static String readSocketMessage(SocketChannel socketChannel) throws IOException {
+        final ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        final int readBytes = socketChannel.read(byteBuffer);
+        byteBuffer.flip();
+        final byte[] bytes = new byte[readBytes];
+        byteBuffer.get(bytes);
+        return new String(bytes, StandardCharsets.UTF_8);
+    }
+
+    static void writeSocketMessage(SocketChannel socketChannel, String message) throws IOException {
+        final byte[] bytes = message.getBytes(StandardCharsets.UTF_8);
+        final ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+        socketChannel.write(byteBuffer);
     }
 }
--- src/test/java/com/ableneo/jdk11to17/ex09unixsocketapi/UnixSocketServerTest.java
+++ src/test/java/com/ableneo/jdk11to17/ex09unixsocketapi/UnixSocketServerTest.java
@@ -1,7 +1,46 @@
 package com.ableneo.jdk11to17.ex09unixsocketapi;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.IOException;
+import java.net.StandardProtocolFamily;
+import java.net.UnixDomainSocketAddress;
+import java.nio.channels.SocketChannel;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class UnixSocketServerTest {
 
-    // TODO switch to jdk 17 branch
+    private static final String SOCKET_PATH = "/tmp/unixSocketServerTest.sock";
+
+    @ParameterizedTest
+    @CsvSource(delimiter = '|',
+            value = {
+                    "John Doe       | Hello, John Doe",
+                    "Snow White     | Hello, Snow White"
+            })
+    void testSocketServer(String name, String expectedResponse) throws IOException, InterruptedException {
+        final String canonicalName = UnixSocketServer.class.getCanonicalName();
+        final String currentDir = System.getProperty("user.dir");
+        final String javaMainClasspathDir = Paths.get(currentDir).resolve("build/classes/java/main").toString();
+        final Process socketServer = new ProcessBuilder("java", "-cp", javaMainClasspathDir, canonicalName, SOCKET_PATH)
+                .start();
+        Thread.sleep(1000);
+
+        final String response;
+        try (final SocketChannel socketChannel = SocketChannel.open(StandardProtocolFamily.UNIX)) {
+            socketChannel.configureBlocking(true);
+            final UnixDomainSocketAddress socketAddress = UnixDomainSocketAddress.of(SOCKET_PATH);
+            socketChannel.connect(socketAddress);
+            UnixSocketServer.writeSocketMessage(socketChannel, name);
+            response = UnixSocketServer.readSocketMessage(socketChannel);
+        }
+
+        socketServer.waitFor();
+        assertThat(socketServer.exitValue()).isZero();
+        assertThat(response).isEqualTo(expectedResponse);
+    }
 
 }
