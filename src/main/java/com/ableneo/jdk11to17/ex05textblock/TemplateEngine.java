e57a203550b90f1afb438e1f438f3a33231589d9
Michal Boska <michal.boska@ableneo.com>
2023-06-08 11:51:08 +0200

    ex05 - Text blocks - jdk17

--- src/main/java/com/ableneo/jdk11to17/ex05textblock/TemplateEngine.java
+++ src/main/java/com/ableneo/jdk11to17/ex05textblock/TemplateEngine.java
@@ -2,17 +2,18 @@ package com.ableneo.jdk11to17.ex05textblock;
 
 public class TemplateEngine {
 
-    private static final String HTML_TEMPLATE = "<html lang=\"en\">\n" +
-            "  <head>\n" +
-            "      <title>E-Shop</title>\n" +
-            "  </head>\n" +
-            "  <body>\n" +
-            "  <h1>Welcome, %s!</h1>\n" +
-            "  </body>\n" +
-            "  </html>";
+    private static final String HTML_TEMPLATE = """
+            <html lang="en">
+            <head>
+                <title>E-Shop</title>
+            </head>
+            <body>
+            <h1>Welcome, %s!</h1>
+            </body>
+            </html>""";
 
     public String getHtmlForUser(String username) {
-        return String.format(HTML_TEMPLATE, username);
+        return HTML_TEMPLATE.formatted(username);
     }
 
 }
