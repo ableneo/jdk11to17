30e9f550b4c4850bdd25e141369b4facf3fdb40b
Michal Boska <michal.boska@ableneo.com>
2023-06-08 17:05:40 +0200

    ex10 - Files api - jdk 11

---
+++ src/main/java/com/ableneo/jdk11to17/ex10filesapi/FilesMismatch.java
@@ -0,0 +1,5 @@
+package com.ableneo.jdk11to17.ex10filesapi;
+
+public class FilesMismatch {
+    // see the test file
+}
---
+++ src/test/java/com/ableneo/jdk11to17/ex10filesapi/FilesMismatchTest.java
@@ -0,0 +1,7 @@
+package com.ableneo.jdk11to17.ex10filesapi;
+
+class FilesMismatchTest {
+
+    // TODO switch to jdk17 branch
+
+}
