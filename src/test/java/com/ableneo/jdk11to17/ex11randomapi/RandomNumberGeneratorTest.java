5d2a3b42e0f3f2a6dbf6e1e04df4cf4913fb7326
Michal Boska <michal.boska@ableneo.com>
2023-06-09 11:26:39 +0200

    ex11 - random api - jdk 17

--- src/test/java/com/ableneo/jdk11to17/ex11randomapi/RandomNumberGeneratorTest.java
+++ src/test/java/com/ableneo/jdk11to17/ex11randomapi/RandomNumberGeneratorTest.java
@@ -2,7 +2,8 @@ package com.ableneo.jdk11to17.ex11randomapi;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Random;
+import java.util.random.RandomGenerator;
+import java.util.random.RandomGeneratorFactory;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,11 +11,20 @@ class RandomNumberGeneratorTest {
 
     private static final int SEED = 123654;
 
-    private final Random random = new Random(SEED);
-
     @Test
     void constantSeedReturnsConstantValue() {
-        assertThat(random.nextInt()).isEqualTo(1897113197);
+        final RandomGenerator randomGenerator = RandomGeneratorFactory.getDefault().create(SEED);
+
+        assertThat(randomGenerator.nextInt()).isEqualTo(715378305);
+    }
+
+    @Test
+    void jumpWithConstantSeedReturnsConstantValue() {
+        RandomGeneratorFactory<RandomGenerator.JumpableGenerator> factory = RandomGeneratorFactory.of("Xoshiro256PlusPlus");
+        final RandomGenerator.JumpableGenerator jumpableGenerator = factory.create(SEED);
+
+        jumpableGenerator.jump();
+        assertThat(jumpableGenerator.nextInt()).isEqualTo(-359988068);
     }
 
 }
