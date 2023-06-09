ab7ffdd767a5b695c6a9f4f9b6f3c53ea5864ca1
Michal Boska <michal.boska@ableneo.com>
2023-06-09 11:09:53 +0200

    ex11 - random api - jdk 11 - add reference to test file

--- src/main/java/com/ableneo/jdk11to17/ex11randomapi/RandomNumberGenerator.java
+++ src/main/java/com/ableneo/jdk11to17/ex11randomapi/RandomNumberGenerator.java
@@ -1,18 +1,7 @@
 package com.ableneo.jdk11to17.ex11randomapi;
 
-import java.util.Random;
-
 public class RandomNumberGenerator {
 
-    private final Random random;
-
-    public RandomNumberGenerator(int seed) {
-        random = new Random(seed);
-    }
-
-    public int nextInt() {
-        return random.nextInt();
-    }
-
+    // See test file
 
 }
